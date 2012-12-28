ac7484025c8436c0b3a59e53b040f61b6e7fbdfc
Kyle Chapman <kyle.chapman@wiggle.co.uk>
2012-12-28 21:15:08 +0000

    Add basic parsing functionality to get a sensible object out of a feature file

--- source/Nucumber/Parsing/GherkinParser.cs
+++ source/Nucumber/Parsing/GherkinParser.cs
@@ -14,7 +14,7 @@ namespace Nucumber.Parsing
 
         public Gherkin Parse(string input)
         {
-            var lines = input.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).Select(ca => ca.ToString());
+            var lines = input.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries).Select(ca => ca.ToString()).ToList();
 
             return new Gherkin
             {
@@ -27,19 +27,21 @@ namespace Nucumber.Parsing
         private string GetTitle(IEnumerable<string> lines)
         {
             var titleLine = lines.First();
-
-            // if (!titleLine.StartsWith("FeatureMarker:")) {throw new Exception("First line of the scenario must start with 'FeatureMarker:'");}
-
             return titleLine.TrimStart(FeatureMarker.ToCharArray());
         }
 
-        private IEnumerable<GherkinScenario> GetScenarios(IEnumerable<string> lines)
+        private IEnumerable<GherkinScenario> GetScenarios(List<string> lines)
         {
             var scenarios = new List<GherkinScenario>();
             var scenarioBuilder = new List<string>();
-            
+
+            var originalCount = lines.Count();
+            var counter = 0;
+
             foreach (var element in lines)
             {
+                counter++;
+
                 if (scenarioBuilder.Count.Equals(0))
                 {
                     if (!element.StartsWith(ScenarioMarker)) continue;
@@ -48,10 +50,10 @@ namespace Nucumber.Parsing
                 }
                 else
                 {
-                    if (element.StartsWith(ScenarioMarker))
+                    if (element.StartsWith(ScenarioMarker) || counter == originalCount)
                     {
                         scenarios.Add(CreateScenarioFrom(scenarioBuilder));
-                        scenarios.Clear();
+                        scenarioBuilder.Clear();
                     }
 
                     scenarioBuilder.Add(element);
@@ -63,29 +65,56 @@ namespace Nucumber.Parsing
 
         private GherkinScenario CreateScenarioFrom(List<string> scenarioBuilder)
         {
-            throw new NotImplementedException();
+            var description = string.Empty;
+            var givens = new List<string>();
+            var whens = new List<string>();
+            var thens = new List<string>();
+
+            var stage = ScenarioBuildingStage.NoneComplete;
+
+            foreach (var line in scenarioBuilder)
+            {
+                if (line.StartsWith(ScenarioMarker))
+                {
+                    description = line.TrimStart(ScenarioMarker.ToCharArray());
+                    stage = ScenarioBuildingStage.TitleComplete;
+                }
+                else
+                {
+                    if (line.StartsWith(GivenMarker) || stage < ScenarioBuildingStage.GivensComplete)
+                    {
+                        givens.Add(line);
+                        stage = ScenarioBuildingStage.TitleComplete;
+                    }
+                    else if (line.StartsWith(WhenMarker) || stage < ScenarioBuildingStage.WhensComplete)
+                    {
+                        whens.Add(line);
+                        stage = ScenarioBuildingStage.GivensComplete;
+                    }
+                    else if (line.StartsWith(ThenMarker) || stage < ScenarioBuildingStage.ThensComplete)
+                    {
+                        thens.Add(line);
+                        stage = ScenarioBuildingStage.WhensComplete;
+                    }
+                }
+            }
+
+            return new GherkinScenario
+            {
+                Description = description,
+                Givens = givens,
+                Whens = whens,
+                Thens = thens
+            };
         }
+    }
 
-        //private GherkinScenario CreateScenarioFrom(List<string> scenarioBuilder)
-        //{
-        //    var stepBuilder = new List<String>();
-
-        //    foreach (var line in scenarioBuilder)
-        //    {
-        //        if (stepBuilder.Count.Equals(0))
-        //        {
-        //            if (line.StartsWith(ScenarioMarker)) { stepBuilder.Add(line); }
-        //        }
-        //        else
-        //        {
-        //            if (line.StartsWith(GivenMarker) || line.StartsWith(WhenMarker) || line.StartsWith(ThenMarker))
-        //            {
-                        
-        //            }
-        //        }
-        //    }
-
-        //    return new GherkinScenario();
-        //}
+    public enum ScenarioBuildingStage
+    {
+        NoneComplete = 0,
+        TitleComplete = 1,
+        GivensComplete = 2,
+        WhensComplete = 3,
+        ThensComplete = 4
     }
 }
--- source/Nucumber/Parsing/GherkinScenario.cs
+++ source/Nucumber/Parsing/GherkinScenario.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace Nucumber.Parsing
 {
     public struct GherkinScenario
     {
         public string Description { get; set; }
+
+        public List<string> Givens { get; set; }
+        public List<string> Whens { get; set; }
+        public List<string> Thens { get; set; }
     }
 }
