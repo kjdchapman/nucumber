864308f97f0002d834eb1fb4efbc265745e012b7
Kyle Chapman <kyle.chapman@wiggle.co.uk>
2012-12-29 22:24:02 +0000

    Complete final versions of the tests for state transition.
    Not 100% happy with these tests or the object, but it'll do to move forward (this was meant to be a quick parser!)

--- source/Nucumber.Tests/GherkinStateTransitions.cs
+++ source/Nucumber.Tests/GherkinStateTransitions.cs
@@ -9,107 +9,188 @@ namespace Nucumber.tests
     [TestFixture]
     public class GherkinStateTransitions
     {
-        [Test]
-        public void Only_a_feature_header_is_valid_to_start_with()
+        private IEnumerable<GherkinLineType> _allLineTypes;
+
+        [SetUp]
+        public void SetUp()
         {
-            var gherkin = new Gherkin();
-            TestStateChange(gherkin, GherkinLineType.FeatureHeader, true);
+            _allLineTypes = Enum.GetValues(typeof(GherkinLineType)).Cast<GherkinLineType>();
+        }
+
+        [Test]
+        public void A_feature_header_is_valid_to_start_with()
+        {
+            foreach (var type in _allLineTypes.Where(type => type == GherkinLineType.FeatureHeader))
+            {
+                var gherkin = new Gherkin();
+
+                TestStartingNewElement(gherkin, type, expectedValid: true);
+            }
         }
         
         [Test]
-        public void Everything_but_a_feature_header_is_invalid_to_start_with()
+        public void Everything_except_a_feature_header_is_not_valid_to_start_with()
         {
-            var gherkin = new Gherkin();
-            TestStateChange(gherkin, LineTypesExcept(GherkinLineType.FeatureHeader), false);
-        }
-
-        [Test]
-        public void Only_scenario_header_and_typeless_line_are_valid_after_a_feature_header()
-        {
-            throw new NotImplementedException();
-        }
-
-        [Test]
-        public void Only_a_Given_When_or_Then_is_valid_after_a_scenario_header()
-        {
-            // TODO: fill in invalid types
-            
-            var gherkin = new Gherkin();
-            gherkin.StartNewElement(GherkinLineType.FeatureHeader);
-            gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
-            
-            Assert.Fail("Test writing not yet complete.");
-        }
-
-        [Test]
-        public void Only_a_When_or_Then_or_And_or_But_or_typeless_line_is_valid_after_a_Given()
-        {
-            // TODO: fill in invalid types
-
-            var gherkin = new Gherkin();
-            gherkin.StartNewElement(GherkinLineType.FeatureHeader);
-            gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
-            gherkin.StartNewElement(GherkinLineType.Given);
-
-            Assert.Fail("Test writing not yet complete.");
-        }
-
-
-        [Test]
-        public void Only_a_Then_or_But_or_typeless_line_is_valid_after_a_When()
-        {
-            // TODO: fill in invalid types
-
-            var gherkin = new Gherkin();
-            gherkin.StartNewElement(GherkinLineType.FeatureHeader);
-            gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
-            gherkin.StartNewElement(GherkinLineType.Given);
-            gherkin.StartNewElement(GherkinLineType.When);
-
-            Assert.Fail("Test writing not yet complete.");
-        }
-
-        [Test]
-        public void Only_a_But_or_scenario_header_or_typeless_line_is_valid_after_a_Then()
-        {
-            // TODO: fill in invalid types
-
-            Assert.Fail("Test writing not yet complete");
-        }
-
-        private List<GherkinLineType> LineTypesExcept(GherkinLineType typeToExclude)
-        {
-            return LineTypesExcept(new List<GherkinLineType> {typeToExclude});
-        }
-
-        private List<GherkinLineType> LineTypesExcept(IEnumerable<GherkinLineType> typesToExclude)
-        {
-            var allLineTypes = Enum.GetValues(typeof(GherkinLineType)).Cast<GherkinLineType>();
-            return allLineTypes.Except(typesToExclude).ToList();
-        }
-
-
-        private void TestStateChange(Gherkin gherkin, GherkinLineType typesUnderTest, bool expectedValid)
-        {
-            TestStateChange(gherkin, new List<GherkinLineType>{typesUnderTest}, expectedValid);
-        }
-
-        private void TestStateChange(Gherkin gherkin, List<GherkinLineType> typesUnderTest, bool expectedValid)
-        {
-            foreach (var type in typesUnderTest)
+            foreach (var type in _allLineTypes.Where(type => type != GherkinLineType.FeatureHeader))
             {
-                try
-                {
-                    gherkin.StartNewElement(type);
-                    Assert.That(expectedValid, Is.EqualTo(true), 
-                        String.Format("No exceptions thrown for state transition to {0} - expected an invalid state transition.", type));
-                }
-                catch (Exception ex)
-                {
-                    Assert.That(expectedValid, Is.EqualTo(false),
-                                string.Format("Exception thrown on state transition to {0} - {1}", type, ex.StackTrace));
-                }
+                var gherkin = new Gherkin();
+
+                TestStartingNewElement(gherkin, type, expectedValid: false);
             }
+        }
+
+        [Test]
+        public void Scenario_header_and_typeless_line_are_valid_after_a_feature_header()
+        {
+            var typeBitmask = GherkinLineType.ScenarioHeader | GherkinLineType.None;
+            var typesToTest = _allLineTypes.Where(type => typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+
+                TestStartingNewElement(gherkin, type, expectedValid: true);
+            }
+        }
+
+        [Test]
+        public void Everything_except_scenario_header_and_typeless_line_are_not_valid_after_a_feature_header()
+        {
+            var typeBitmask = GherkinLineType.ScenarioHeader | GherkinLineType.None;
+            var typesToTest = _allLineTypes.Where(type => !typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+
+                TestStartingNewElement(gherkin, type, expectedValid: false);
+            }
+        }
+
+        [Test]
+        public void Given_When_and_Then_are_valid_after_a_scenario_header()
+        {
+            var typeBitmask = GherkinLineType.Given | GherkinLineType.When | GherkinLineType.Then;
+            var typesToTest = _allLineTypes.Where(type => typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+                gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
+    
+                TestStartingNewElement(gherkin, type, expectedValid: true);
+            }
+        }
+
+        [Test]
+        public void Everything_except_Given_When_and_Then_are_not_valid_after_a_scenario_header()
+        {
+            var typeBitmask = GherkinLineType.Given | GherkinLineType.When | GherkinLineType.Then;
+            var typesToTest = _allLineTypes.Where(type => !typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+                gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
+
+                TestStartingNewElement(gherkin, type, expectedValid: true);
+            }
+        }
+
+
+        [Test]
+        public void Then_But_and_typeless_line_are_valid_after_a_When()
+        {
+            var typeBitmask = GherkinLineType.Then | GherkinLineType.But | GherkinLineType.None;
+            var typesToTest = _allLineTypes.Where(type => typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+                gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
+                gherkin.StartNewElement(GherkinLineType.Given);
+                gherkin.StartNewElement(GherkinLineType.When);
+
+                TestStartingNewElement(gherkin, type, expectedValid: true);
+            }
+        }
+
+        [Test]
+        public void Everything_except_Then_But_And_typeless_line_are_not_valid_after_a_scenario_header()
+        {
+            var typeBitmask = GherkinLineType.Then | GherkinLineType.But | GherkinLineType.None;
+            var typesToTest = _allLineTypes.Where(type => !typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+                gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
+                gherkin.StartNewElement(GherkinLineType.Given);
+                gherkin.StartNewElement(GherkinLineType.When);
+
+                TestStartingNewElement(gherkin, type, expectedValid: false);
+            }
+        }
+
+        [Test]
+        public void But_and_scenario_header_and_typeless_line_are_valid_after_a_Then()
+        {
+            var typeBitmask = GherkinLineType.But | GherkinLineType.ScenarioHeader | GherkinLineType.None;
+            var typesToTest = _allLineTypes.Where(type => typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+                gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
+                gherkin.StartNewElement(GherkinLineType.Given);
+                gherkin.StartNewElement(GherkinLineType.When);
+                gherkin.StartNewElement(GherkinLineType.Then);
+
+                TestStartingNewElement(gherkin, type, expectedValid: true);
+            }
+        }
+
+        [Test]
+        public void Everything_except_But_and_scenario_header_and_typeless_line_are_not_valid_after_a_Then()
+        {
+            var typeBitmask = GherkinLineType.But | GherkinLineType.ScenarioHeader | GherkinLineType.None;
+            var typesToTest = _allLineTypes.Where(type => !typeBitmask.HasFlag(type));
+
+            foreach (var type in typesToTest)
+            {
+                var gherkin = new Gherkin();
+                gherkin.StartNewElement(GherkinLineType.FeatureHeader);
+                gherkin.StartNewElement(GherkinLineType.ScenarioHeader);
+                gherkin.StartNewElement(GherkinLineType.Given);
+                gherkin.StartNewElement(GherkinLineType.When);
+                gherkin.StartNewElement(GherkinLineType.Then);
+
+                TestStartingNewElement(gherkin, type, expectedValid: false);
+            }
+        }
+
+        private void TestStartingNewElement(Gherkin gherkin, GherkinLineType type, bool expectedValid)
+        {
+            try
+            {
+                gherkin.StartNewElement(type);
+
+                Assert.That(expectedValid, Is.EqualTo(true), 
+                    String.Format("No exceptions thrown for state transition to {0} - expected an invalid state transition.", type));
+            }
+            catch (Exception ex)
+            {
+                Assert.That(expectedValid, Is.EqualTo(false),
+                    string.Format("Exception thrown on state transition to {0} - {1}", type, ex.StackTrace));
+            }
+            
         }
     }
 }
